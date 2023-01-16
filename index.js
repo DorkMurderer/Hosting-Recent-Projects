2d5855c37bb839d917295522175b742b61c3ba3f
DorkMurderer <78510253+DorkMurderer@users.noreply.github.com>
2023-01-15 19:20:29 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/new-todo-app/")
+})
+
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/Fixed-animal-trivia/")
+})
+
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/Calculator/")
+})
+
+document.getElementsByClassName("article")[3].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/TimerApp/")
+})
+
+document.getElementsByClassName("article")[4].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/New-Year-Countdown/")
+})
+document.getElementsByClassName("article")[5].addEventListener("click", function(){
+    window.open("https://dorkmurderer.github.io/review-submitting-gui-front-end/")
+})
